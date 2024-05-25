a298787ae35d493e964c976c230b95cf33ff200e
HemanthPatil547 <167996638+HemanthPatil547@users.noreply.github.com>
2024-05-25 11:33:20 +0530

    Loops

---
+++ src/nestIf/N5.java
@@ -0,0 +1,21 @@
+package nestIf;
+
+public class N5 {
+
+	public static void main(String[] args) {
+				
+		int c =0;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j =1;j<=i;j++)
+			{
+				c++;
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
---
+++ src/nestIf/Nst1.java
@@ -0,0 +1,17 @@
+package nestIf;
+
+public class Nst1 {
+
+	public static void main(String[] args) 
+	{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j =1;j<6;j++)
+		{
+			System.out.print(j+i+"");
+		}
+		System.out.println();
+	}
+	}
+
+}
